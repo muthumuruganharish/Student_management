389a53521afc65ee943470cb9d06315428924fa2
harish <muthumuruganharish272003@gmail.com>
2026-02-27 14:43:31 +0530

    project added

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
